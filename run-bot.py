07ad1c09ac0ad642ad7e18e98c4f6a1ab271bd0d
Mathieu Leplatre <mathieu@mozilla.com>
2015-12-04 12:30:39 +0100

    Add some safety checks for demo server

--- run-bot.py
+++ run-bot.py
@@ -10,15 +10,18 @@ import requests
 import telepot
 
 
+DEMO_SERVER_URL = "https://kinto.dev.mozaws.net/v1"
 TOKEN = os.getenv("TOKEN")
-SERVER_URL = os.getenv("SERVER_URL", "https://kinto.dev.mozaws.net/v1")
+SERVER_URL = os.getenv("SERVER_URL", DEMO_SERVER_URL)
 SERVER_AUTH = os.getenv("SERVER_AUTH", "botuser:secret")
 BUCKET = os.getenv("BUCKET", "kintobot")
 COLLECTION = os.getenv("COLLECTION", "wall")
 DOWNLOAD_PATH = os.getenv("DOWNLOAD_PATH", ".")
+CONTACT_SUPPORT = os.getenv("CONTACT_SUPPORT", "@leplatrem")
 
 DOWNLOAD_TYPES = ("voice", "sticker", "photo", "audio", "document", "video")
 BUCKET_PERMISSIONS = {"collection:create": ["system.Authenticated"]}
+COLLECTION_PERMISSIONS = {"record:create": ["system.Authenticated"]}
 RECORD_PERMISSIONS = {"read": ["system.Everyone"]}
 THUMB_UP = u'\U0001f44d'
 
@@ -32,13 +35,13 @@ def kinto_init():
     try:
         kinto.create_bucket(BUCKET, permissions=BUCKET_PERMISSIONS)
     except kinto_exceptions.KintoException as e:
-        if e.response.status_code != 412:
+        if e.response.status_code not in (403, 412):
             raise e
         print("Bucket '%s' already exists." % BUCKET)
     try:
-        kinto.create_collection(COLLECTION, bucket=BUCKET)
+        kinto.create_collection(COLLECTION, bucket=BUCKET, permissions=COLLECTION_PERMISSIONS)
     except kinto_exceptions.KintoException as e:
-        if e.response.status_code != 412:
+        if e.response.status_code not in (403, 412):
             raise e
         print("Collection '%s' already exists." % COLLECTION)
 
@@ -60,7 +63,7 @@ def kinto_create_attachment(record, tmpfile, filename, mimetype):
     pprint.pprint(response.json())
 
 
-def download_from_telegram(attachment):
+def download_from_telegram(chat_id, attachment):
     file_id = attachment["file_id"]
 
     filename = attachment.get("file_name")
@@ -101,22 +104,33 @@ def handle(msg):
         bot.sendMessage(chat_id, welcome)
         return
 
+    # Since demo server is flushed every morning, recreate bucket/collection!
+    if SERVER_URL == DEMO_SERVER_URL:
+        kinto_init()
+
     # Attributes sent to Kinto.
     record = {"date": msg["date"],
               "from": {"first_name": msg["from"]["first_name"]}}
     content = msg[msg_type]
 
-    if msg_type not in DOWNLOAD_TYPES:
-        record[msg_type] = content
-        kinto_create_record(record)
-    else:
-        if msg_type == "photo":
-            content = content[-1]
+    tmpfile = None
+    try:
+        if msg_type not in DOWNLOAD_TYPES:
+            record[msg_type] = content
+            kinto_create_record(record)
+        else:
+            if msg_type == "photo":
+                content = content[-1]
 
-        tmpfile, filename, mimetype = download_from_telegram(content)
-        kinto_create_attachment(record, tmpfile, filename, mimetype)
-        os.remove(tmpfile)
-    bot.sendMessage(chat_id, THUMB_UP)
+            tmpfile, filename, mimetype = download_from_telegram(chat_id, content)
+            kinto_create_attachment(record, tmpfile, filename, mimetype)
+        bot.sendMessage(chat_id, THUMB_UP)
+    except Exception as e:
+        print(e)
+        bot.sendMessage(chat_id, "An error occured. Contact %s" % CONTACT_SUPPORT)
+    finally:
+        if tmpfile and os.path.exists(tmpfile):
+            os.remove(tmpfile)
 
 
 if __name__ == "__main__":
